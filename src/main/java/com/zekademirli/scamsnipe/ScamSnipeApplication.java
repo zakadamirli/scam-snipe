79d78ec6c0faefe808185f87e38569fe1e237dd3
zakadamirli <z3kkadamirli@gmail.com>
2025-07-16 14:50:10 +0400

    feat: first commit

---
+++ src/main/java/com/zekademirli/scamsnipe/ScamSnipeApplication.java
@@ -0,0 +1,13 @@
+package com.zekademirli.scamsnipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScamSnipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScamSnipeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zekademirli/scamsnipe/ScamSnipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zekademirli.scamsnipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScamSnipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
