6263b07e73f61d00ca4d5e02370d3bcf2f274c5e
zakadamirli <z3kkadamirli@gmail.com>
2025-07-17 20:05:32 +0400

    feat: create order service

---
+++ order-service/src/test/java/com/zekademirli/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zekademirli.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
