12a842ddd0e814ab31274b593b54448661a3a8af
zakadamirli <z3kkadamirli@gmail.com>
2025-07-18 00:55:51 +0400

    feat: add OrderProducer

--- order-service/src/main/java/com/zekademirli/orderservice/kafka/OrderProducer.java
+++ order-service/src/main/java/com/zekademirli/orderservice/kafka/OrderProducer.java
@@ -1,4 +1,21 @@
 package com.zekademirli.orderservice.kafka;
 
+import com.zekademirli.orderservice.dto.OrderRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class OrderProducer {
+
+    private final KafkaTemplate<String, OrderRequest> kafkaTemplate;
+
+    @Value("${order.topic}")
+    private String topic;
+
+    public void sendOrder(OrderRequest orderRequest) {
+        kafkaTemplate.send(topic, orderRequest.getOrderId(), orderRequest);
+    }
 }
