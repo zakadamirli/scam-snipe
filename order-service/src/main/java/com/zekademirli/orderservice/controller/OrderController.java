dca35184d2f1268d66b58bd408fbbc77986efba7
zakadamirli <z3kkadamirli@gmail.com>
2025-07-18 00:54:49 +0400

    feat: add controller for create info(for kafka)

--- order-service/src/main/java/com/zekademirli/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/zekademirli/orderservice/controller/OrderController.java
@@ -1,4 +1,27 @@
 package com.zekademirli.orderservice.controller;
 
+import com.zekademirli.orderservice.dto.OrderRequest;
+import com.zekademirli.orderservice.model.Order;
+import com.zekademirli.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/orders")
+@RequiredArgsConstructor
 public class OrderController {
+
+    private final OrderService orderProducer;
+
+    @GetMapping("/ping")
+    public ResponseEntity<String> ping() {
+        return ResponseEntity.ok("OK");
+    }
+
+    @PostMapping
+    public ResponseEntity<Order> createOrder(@RequestBody OrderRequest request) {
+        Order createOrder = orderProducer.createOrder(request);
+        return ResponseEntity.ok(createOrder);
+    }
 }
